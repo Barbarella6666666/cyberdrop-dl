ad35a3cb2eef35f4e2acaeb5655feb5651dbb2dd
Barbarella <barbarella6666666@protonmail.com>
2026-09-04 08:24:29 -0300

    adding tests for videos and photos

---
+++ tests/crawlers/test_cases/bluesky.py
@@ -0,0 +1,33 @@
+DOMAIN = "bluesky"
+TEST_CASES = [
+    {
+        "url": "https://bsky.app/profile/snarkysillysad.bsky.social/post/3muosmesdwc2k",
+        "results": [
+            {
+                "url": "https://video.bsky.app/watch/did%3Aplc%3Asg6jg3i3m4hlkw3fqk6hw7sq/bafkreif7r6ewuwlzzcqkwniy6h247ljmfscqkdidkebo56fb4mii76iloa/playlist.m3u8",
+                "filename": "3muosmesdwc2k [1280p].mp4",
+                "debrid_url": None,
+                "original_filename": "3muosmesdwc2k",
+                "referer": "https://bsky.app/profile/snarkysillysad.bsky.social/post/3muosmesdwc2k",
+                "album_id": None,
+                "uploaded_at": 1788519486,
+                "download_folder": "re:@snarkysillysad.bsky.social (Bluesky)/2026-09-04 - 3muosmesdwc2k",
+            },
+        ],
+    },
+    {
+        "url": "https://bsky.app/profile/pmamtraveller.bsky.social/post/3muolshmb2c2g",
+        "results": [
+            {
+                "url": "https://cdn.bsky.app/img/feed_fullsize/plain/did:plc:vt67jrpurra7lod3gj44snaw/bafkreicriek7hgvmo6zvf44gkhzbycnhrmjix33k5kdtg52kippvktfqyu",
+                "filename": "bafkreicriek7hgvmo6zvf44gkhzbycnhrmjix33k5kdtg52kippvktfqyu.jpg",
+                "debrid_url": None,
+                "original_filename": "bafkreicriek7hgvmo6zvf44gkhzbycnhrmjix33k5kdtg52kippvktfqyu.jpg",
+                "referer": "https://bsky.app/profile/pmamtraveller.bsky.social/post/3muolshmb2c2g",
+                "album_id": None,
+                "uploaded_at": 1788512174,
+                "download_folder": "re:@pmamtraveller.bsky.social (Bluesky)/2026-09-04 - 3muolshmb2c2g",
+            },
+        ],
+    },
+]
